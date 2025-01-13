da6399d3148847324428a270ef1169ebf698b1d5
EroInnovations <eroinnovations9@gmail.com>
2025-01-13 03:54:09 +0300

    Prelease
    Version 0

--- Library/Functions/Body/Body.js
+++ Library/Functions/Body/Body.js
@@ -1,4 +1,11 @@
 const BODY=()=>{
 
+    const ELEMENT=document.querySelector('body');
+
+    ELEMENT.style.overflow='hidden';
+    ELEMENT.style.position='fixed';
+    ELEMENT.style.width='100%';
+    ELEMENT.style.height='100%';
+ 
 };
 export{BODY};
