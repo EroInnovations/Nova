f754afc954b21c9270cdccecb356d1b415130682
EroInnovations <eroinnovations9@gmail.com>
2025-01-13 00:05:18 +0000

    Nova Pre-lease

--- Model/AutoRun.js
+++ Model/AutoRun.js
@@ -1,21 +1,55 @@
 const AUTORUN=()=>{
 const DATA=`const AUTORUN=()=>{
-    import('../Start/Start.js')
-    .then(module =>
-        {
-            if (typeof module.START === 'function') {
-                module.START();
-            } else {
-                console.error('START is not defined in the module');
+
+    if (localStorage.getItem('Environment') === 'Development') {
+
+        const LINK='../Start/Start.js';
+
+        import(LINK)
+
+        .then(module =>
+            {
+                if (typeof module.START === 'function') {
+                    module.START();
+                } else {
+                    console.error('START is not defined in the module');
+                    }
+                }
+            )
+
+            .catch(error =>
+                {
+                    console.error('Error loading the module:', error);
+                }
+            );
+            
+        } 
+
+    else {
+            
+        const LINK='https://eroinnovations.github.io/Nova/Start/Start.js'
+
+        import(LINK)
+
+        .then(module =>
+            {
+                if (typeof module.START === 'function') {
+                        module.START();
+                    } else {
+                        console.error('START is not defined in the module');
                 }
             }
         )
-    .catch(error =>
-        {
+
+        .catch(error =>
+            {
                 console.error('Error loading the module:', error);
-        }
-    );  
-};  
+            }
+        );
+            
+    };
+
+};
 `;
 localStorage.setItem('AUTORUN',DATA);
 };
