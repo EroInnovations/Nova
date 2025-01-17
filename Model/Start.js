1a74f9abb4e888807bc59f89bc1f42a83a649382
EroInnovations <eroinnovations9@gmail.com>
2025-01-17 11:20:07 +0300

    Add files via upload

--- Model/Start.js
+++ Model/Start.js
@@ -2,24 +2,52 @@ const NOVASTART=()=>{
     const DATA={
         "spreadsheetUrl":"https://docs.google.com/spreadsheets/d/1kd15tCp1cX6TIUSsm3GcrfxDvOrmqlTNxAaseR8LBhw/edit?gid=0#gid=0",
         "sheetName":"AppManager"
-    }
+    };
 
     fetch('https://script.google.com/macros/s/AKfycby5S_I3_hu6iEB0n8I7cmFZ9liA1kGPpQ-9ewmTcJmkUuMCYY540oaGDwJGE4ADs6tv5w/exec',{
         method:"Post",
         body:JSON.stringify(DATA)
     })
+
     .then(res =>res.json())
+
     .then(data =>{
+
         data.forEach(element => {   
+
             if (element.ID === localStorage.getItem('NAME')) {
 
-                console.log(element);
+                localStorage.setItem("PROJECT",element.Functions);
+
+                if (!localStorage.getItem("Updates")) {
+
+                    setTimeout(() => {
+
+                        if (localStorage.getItem('Environment')==='Production') {
+    
+                            Android.reloadPage();
+    
+                        }else{
+    
+                            location.href='./index.html';
+    
+                        };
+                        
+                    }, 2000);
+
+                    localStorage.setItem("Updates",'Nova');
+                    
+                };
 
             } ;
         });
+
     } )
+
     .catch(error =>{
+
         console.log(error);
+
     });
 
 };
