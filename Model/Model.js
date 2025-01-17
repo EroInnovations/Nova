6371622a8d4394f5aac8d6fafcb1f2472143f35c
EroInnovations <eroinnovations9@gmail.com>
2025-01-17 11:27:50 +0300

    Add files via upload

--- Model/Model.js
+++ Model/Model.js
@@ -2,12 +2,12 @@ import { AUTORUN } from "./AutoRun.js";
 import { AUTOSTART } from "./AutoStart.js";
 const NOVA=()=>{
 const DATA=`ROUTEJS(localStorage.getItem('FUNCTIONS'));
+ROUTEJS(localStorage.getItem('PROJECT'));
 ROUTEJS(localStorage.getItem('AUTORUN'));
 ROUTEJS(localStorage.getItem('AUTOSTART'));
 ROUTEJS(localStorage.getItem('COMPONENTS'));
 ROUTEJS(localStorage.getItem('ICONS'));
 ROUTEJS(localStorage.getItem('APIS'));
-ROUTEJS(localStorage.getItem('PROJECT'));
 `;
 localStorage.setItem('NOVA',DATA);
 localStorage.setItem('Updates','Nova');
