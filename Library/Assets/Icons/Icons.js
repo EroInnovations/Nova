16bfcdfe8c956f4a0674287855cd1ad793c799b8
EroInnovations <eroinnovations9@gmail.com>
2025-01-19 09:29:53 +0300

    Add files via upload

--- Library/Assets/Icons/Icons.js
+++ Library/Assets/Icons/Icons.js
@@ -3,6 +3,7 @@ import { ADDICON } from "./WhiteIcons/AddIcon/Addicon.js";
 import { BACKICON } from "./WhiteIcons/BackIcon/BackIcon.js";
 import { INFOICON } from "./WhiteIcons/InfoIcon/InfoIcon.js";
 import { MENUICON } from "./WhiteIcons/MenuIcon/MenuIcon.js";
+import { SHOPPINGCART } from "./WhiteIcons/ShoppingCart/ShoppingCart.js";
 
 
 const ICONS=()=>{
@@ -12,6 +13,7 @@ const WHITEMENUICON='${MENUICON}';
 const WHITEADDICON='${ADDICON}';  
 const WHITEBACKICON='${BACKICON}'; 
 const WHITEINFOICON='${INFOICON}'; 
+const WHITESHOPPINGCART='${SHOPPINGCART}';
 `;
     localStorage.setItem("ICONS",DATA);
 
@@ -19,6 +21,6 @@ const WHITEINFOICON='${INFOICON}';
 export{ICONS};
 
 `
-const WHITEINFOICON='${INFOICON}';
+const WHITESHOPPINGCART='${SHOPPINGCART}';
 
 `
