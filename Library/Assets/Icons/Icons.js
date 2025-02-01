1c588db093d43b18d11d9454a7e75a63285bcccf
EroInnovations <eroinnovations9@gmail.com>
2025-01-31 20:34:53 -0800

    Major Updates

--- Library/Assets/Icons/Icons.js
+++ Library/Assets/Icons/Icons.js
@@ -1,19 +1,22 @@
-import { ADDPERSONICON } from "./AddPersonIcon/AddPersonIcon.js";
-import { ANDROIDICON } from "./AndroidIcon/AndroidIcon.js";
-import { APPICON } from "./AppIcon/AppIcon.js";
-import { BAGICON } from "./BagIcon/BagIcon.js";
-import { BICYCLEICON } from "./BicycleIcon/BicycleIcon.js";
-import { DROPDOWNICON } from "./DropDownIcon/DropDownIcon.js";
-import { HOMEICON } from "./HomeIcon/HomeIcon.js";
-import { LEFTBACKICON } from "./LeftBackIcon/LeftBackIcon.js";
-import { FISKONICON } from "./Logos/fiskon/fiskon.js";
-import { WINKSTA } from "./Logos/Winksta/Winksta.js";
-import { SONGBACKICON } from "./SongBackIcon/SongBackIcon.js";
+import { ADDPERSONICON } from "./WhiteIcons/AddPersonIcon/AddPersonIcon.js";
+import { ANDROIDICON } from "./WhiteIcons/AndroidIcon/AndroidIcon.js";
+import { APPICON } from "./WhiteIcons/AppIcon/AppIcon.js";
+import { BAGICON } from "./WhiteIcons/BagIcon/BagIcon.js";
+import { BICYCLEICON } from "./WhiteIcons/BicycleIcon/BicycleIcon.js";
+import { DROPDOWNICON } from "./WhiteIcons/DropDownIcon/DropDownIcon.js";
+import { HOMEICON } from "./WhiteIcons/HomeIcon/HomeIcon.js";
+import { LEFTBACKICON } from "./WhiteIcons/LeftBackIcon/LeftBackIcon.js";
+import { COMMUNITYRISELOGO } from "./WhiteIcons/Logos/CommunityRise/CommunityRise.js";
+import { FISKONICON } from "./WhiteIcons/Logos/fiskon/fiskon.js";
+import { WINKSTA } from "./WhiteIcons/Logos/Winksta/Winksta.js";
+import { SONGBACKICON } from "./WhiteIcons/SongBackIcon/SongBackIcon.js";
 import { ADDICON } from "./WhiteIcons/AddIcon/Addicon.js";
 import { BACKICON } from "./WhiteIcons/BackIcon/BackIcon.js";
 import { INFOICON } from "./WhiteIcons/InfoIcon/InfoIcon.js";
 import { MENUICON } from "./WhiteIcons/MenuIcon/MenuIcon.js";
 import { SHOPPINGCART } from "./WhiteIcons/ShoppingCart/ShoppingCart.js";
+import { WHATSAPPICON } from "./WhiteIcons/WhatsAppIcon/WhatsAppIcon.js";
+import { INSTAGRAMICON } from "./WhiteIcons/InstagramIcon/InstagramIcon.js";
 const ICONS=()=>{
 const DATA=`const FISKONICON='${FISKONICON}';
 const WHITEMENUICON='${MENUICON}';
@@ -31,6 +34,8 @@ const WHITEBAGICON='${BAGICON}';
 const WHITEBICYCLEICON='${BICYCLEICON}';
 const WHITEDROPDOWNICON='${DROPDOWNICON}';
 const WHITEHOMEICON='${HOMEICON}';
+const COMMUNITYRISELOGO='${COMMUNITYRISELOGO}';
+const WHITEINSTAGRAMICON='${INSTAGRAMICON}';
 `;
     localStorage.setItem("ICONS",DATA);
 
@@ -38,6 +43,6 @@ const WHITEHOMEICON='${HOMEICON}';
 export{ICONS};
 
 `
-const WHITEHOMEICON='${HOMEICON}';
+const WHITEINSTAGRAMICON='${INSTAGRAMICON}';
 
 `
