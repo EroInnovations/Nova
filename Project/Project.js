bae7c318f50c527331305eff8a222f8481a4a42b
EroInnovations <eroinnovations9@gmail.com>
2025-03-12 20:15:18 +0300

    Update Project.js
    The Main Projects Are Being Served

--- Project/Project.js
+++ Project/Project.js
@@ -2,6 +2,10 @@ const API='https://docs.google.com/spreadsheets/d/1Utfr1wkoZSRvM9TOKaTxOX6orYE8A
 
 const NOVASTART=()=>{
 
+    APPDOWNLOAD(()=>{
+
+    });
+
     ACCOUNTCHECKER(()=>{
 
         ROUTE('',HOMEPAGE,'HOMEPAGE');
@@ -20,33 +24,43 @@ const NOVASTART=()=>{
 
 const HOMEPAGE=()=>{
 
-    HOMEHEADERTEMPLATE('','#333333','',(ELEMENT)=>{
+    DEJSON(localStorage.getItem('UserData'),(data)=>{
 
-        LEFTIMAGE(ELEMENT,WHITEMENUICON,'30px','25px','1rem','',(ELEMENTS)=>{
+        HOMEHEADERTEMPLATE('','#333333','',(ELEMENT)=>{
 
-            FULLMENUTEMPLATE('','#333333','50%','left',(ELEMENT)=>{
-
-                MENUPAGE(ELEMENT);
-
+            LEFTIMAGE(ELEMENT,WHITEMENUICON,'30px','25px','1rem','',(ELEMENTS)=>{
+    
+                FULLMENUTEMPLATE('','#333333','50%','left',(ELEMENT)=>{
+    
+                    MENUPAGE(ELEMENT);
+    
+                });
+            
             });
-        
+    
+            CENTERTEXT(ELEMENT,'','App Manager','','20px','',()=>{
+    
+            });
+    
+            RIGHTIMAGE(ELEMENT,WHITEUSERICON,'30px','30px','1rem','',(ELEMENTS)=>{
+    
+            });
+    
+        },(ELEMENT)=>{
+    
+            USERDATA(ELEMENT,data.ID,'');
+    
         });
-
-        CENTERTEXT(ELEMENT,'','App Manager','','20px','',()=>{
-
-        });
-
-        RIGHTIMAGE(ELEMENT,WHITEUSERICON,'30px','30px','1rem','',(ELEMENTS)=>{
-
-        });
-
-    },()=>{
 
     });
     
 };
 
 const LOGINPAGE=()=>{
+
+    DELETEDATA('','UserEmail');
+
+    DELETEDATA('','UserPassword');
 
     CLEAR('');
 
@@ -112,11 +126,14 @@ const LOGINPAGE=()=>{
 
         FOOTER(ELEMENT,' ','','80px',(ELES)=> {
 
-            LEFTTEXT(ELES,'','Forgot Password !' ,'','25px','5rem','',()=>{
+            LEFTTEXT(ELES,'','Forgot Password !' ,'','25px','5rem','',(ELEMSE)=>{
+
+                ROUTE(' ',FORGOTPASSWORDPAGE,'LOGINPAGE');
 
             });
 
-            CENTERTEXT(ELES,'','||','forestgreen','40px','',()=>{
+            CENTERTEXT(ELES,'','||','forestgreen','40px','',(ELEMSE)=>{
+
 
             });
 
@@ -134,6 +151,12 @@ const LOGINPAGE=()=>{
 
 const CREATEACCOUNTPAGE=()=>{
 
+    DELETEDATA('','UserName');
+
+    DELETEDATA('','UserEmail');
+
+    DELETEDATA('','UserPassword');
+
     CLEAR('');
 
     CENTERVIEW('','#33333350',(ELEMENT)=>{
@@ -148,25 +171,31 @@ const CREATEACCOUNTPAGE=()=>{
 
         BREAK(ELEMENT);BREAK(ELEMENT);
 
-        INPUT(ELEMENT,'text','','Enter User Name',()=>{
+        INPUT(ELEMENT,'text','','Enter User Name',(data)=>{
+
+            STOREDATA('','UserName',data);
 
         });
 
         BREAK(ELEMENT);BREAK(ELEMENT);
 
-        INPUT(ELEMENT,'email','','Enter User Email',()=>{
+        INPUT(ELEMENT,'email','','Enter User Email',(data)=>{
+
+            STOREDATA('','UserEmail',data);
 
         });
 
         BREAK(ELEMENT);BREAK(ELEMENT);
 
-        INPUT(ELEMENT,'password','','Enter User Password',()=>{
+        INPUT(ELEMENT,'password','','Enter User Password',(data)=>{
+
+            STOREDATA('','UserPassword',data);
 
         });
 
         BREAK(ELEMENT);BREAK(ELEMENT);
 
-        BUTTON(ELEMENT,'80%','50px','forestgreen','','Sign Up',()=>{
+        BUTTON(ELEMENT,'80%','50px','forestgreen','','Sign Up',(ELEMENT)=>{
 
             CONDITION(sessionStorage.getItem('UserName'),()=>{
 
@@ -217,13 +246,89 @@ const CREATEACCOUNTPAGE=()=>{
 
 };
 
+const FORGOTPASSWORDPAGE=()=>{
+
+    DELETEDATA('','UserEmail');
+
+    DELETEDATA('','UserPassword');
+
+    CLEAR('');
+
+    CENTERVIEW('','#33333350',(ELEMENT)=>{
+
+        STYLED(ELEMENT,'border-radius','10px');
+
+        BREAK(ELEMENT);BREAK(ELEMENT);
+
+        IMAGE(ELEMENT,WHITEFOLDERICON,'50%','40%','',()=>{
+
+        });
+
+        BREAK(ELEMENT);BREAK(ELEMENT);BREAK(ELEMENT);BREAK(ELEMENT);
+
+        INPUT(ELEMENT,'email','','Enter User Email',(data)=>{
+
+            STOREDATA('','UserEmail',data);
+
+        });
+
+        BREAK(ELEMENT);BREAK(ELEMENT);
+
+        BUTTON(ELEMENT,'80%','50px','forestgreen','','Recover',(ELEMSTS)=>{
+
+            CONDITION(sessionStorage.getItem('UserEmail'),()=>{
+
+                CLOUDLOGINTEMPLATE(ELEMSTS,API,'Users','Please Wait','UserEmail',sessionStorage.getItem('UserEmail'),'UserPassword',sessionStorage.getItem('UserPassword'),'Sign In','No User Account Found','Wrong User Password',(data)=>{
+
+                    JSONIFICATION(data,(MyData)=>{
+
+                        STOREDATA(' ','UserData',MyData);
+
+                        ROUTE('',HOMEPAGE,'HOMEPAGE');
+
+                    });
+
+                });
+
+            },()=>{
+
+                MESSAGEDISPLAY('','Enter User Email','');
+
+            });
+
+        });
+
+        FOOTER(ELEMENT,' ','','80px',(ELES)=> {
+
+            LEFTTEXT(ELES,'','Log In !' ,'','25px','5rem','',()=>{
+
+                ROUTE('',LOGINPAGE,'LOGINPAGE');
+
+            });
+
+            CENTERTEXT(ELES,'','||','forestgreen','40px','',()=>{
+
+            });
+
+            RIGHTTEXT(ELES,'','Create Account ?','','25px','5rem','',(ELEMSE)=>{
+
+                ROUTE(' ',CREATEACCOUNTPAGE,'LOGINPAGE');
+
+            });
+
+        });
+
+    });
+
+};
+
 const MENUPAGE=(ELEMENT)=>{
 
     BUTTONIMAGE(ELEMENT,'forestgreen','New Project','',WHITEPOSTICON,'50px',()=>{
 
     });
 
-    FOOTER(ELEMENT,'red','','',(ELES)=> {
+    FOOTER(ELEMENT,' ','','60px',(ELES)=> {
 
         IMAGEBUTTON(ELES,'forestgreen','Log Out','',WHITENEXTICON,'50px',()=>{
 
@@ -235,4 +340,221 @@ const MENUPAGE=(ELEMENT)=>{
 
     });
 
+};
+
+const APPDOWNLOAD=(callback)=>{
+
+    CHECKER(navigator.onLine,()=>{
+        
+        GETDATA(API,'APPMANAGER',(data)=>{
+
+            const MYDATA={
+                    'Name':'AppManager',
+                    'data':data,
+                }     
+            STOREINDEXED('AppManager', 'AppManager', MYDATA, (data)=>{
+
+                CHECKER(data === false,()=>{
+
+                    UPDATEINDEX('AppManager', 'AppManager', MYDATA,(datata)=>{
+
+                        callback();
+
+                    });
+
+                });
+
+            });
+            
+        },(data)=>{
+
+            console.log(data);
+
+        });
+
+    });
+
+};
+
+const USERDATA=(ELEMENT,Name,Delete)=>{
+
+    CONDITION(Delete,()=>{
+
+        CLEAR(ELEMENT);
+
+        GETINDEXEDDATA ('AppManager', 'AppManager', (data)=>{
+
+            CHECKER( data.Owner === Name && data.AppDeleted ,()=>{
+
+                VIEW(ELEMENT,' ','46%','35%','',(ELEMENTS)=>{
+    
+                    STYLED(ELEMENTS,'display','inline-table');
+                    STYLED(ELEMENTS,'margin','1.5%');
+                    STYLED(ELEMENTS,'border','1px solid forestgreen');
+    
+                    ICON(ELEMENTS,WHITECHECKICON,'25px','25px','',(ELEMENTSE)=>{
+    
+                        STYLED(ELEMENTSE,'position','absolute');
+                        STYLED(ELEMENTSE,'top','5%');
+                        STYLED(ELEMENTSE,'right','5%');
+                        STYLED(ELEMENTSE,'background','forestgreen');
+                        STYLED(ELEMENTSE,'padding','2%');
+                        STYLED(ELEMENTSE,'border-radius','5px');
+    
+                        CLICK(ELEMENTSE,()=>{
+
+                            CONDITION(navigator.onLine,()=>{
+
+                                MESSAGEDISPLAY('',`${data.AppName} is Being Restored`,'');
+    
+                                const INFO=[data.AppName,data.AppDescription,data.AppColors,data.AppConfiguration,data.AppCreatedOn,data.AppVersion,'',data.AppKeyWord,data.AppPackageName,data.AppCompany,data.AndroidDesign,data.AndroidFunctions,data.DesktopDesign,data.DesktopFunctions,data.WebDesign,data.WebFunctions,data.SharedDesign,data.SharedFunctions,data.AppLogic,data.AppRegion,data.AppState,data.AppCatergory,data.AppIcon,data.UpdatedOn,data.Owner];
+    
+                                UPDATEDATA(API,'APPMANAGER',data.ID,INFO,()=>{
+        
+                                    APPDOWNLOAD(()=>{
+        
+                                        ROUTE('',HOMEPAGE,'HOMEPAGE');
+        
+                                    });
+        
+                                },()=>{
+        
+                                        MESSAGEDISPLAY('','Failed to Delete App','');
+        
+                                    }
+    
+                                );
+
+                            },()=>{
+
+                                NOINTERNETTEMPLATE();
+
+                            });
+        
+                        });
+                
+                    });
+            
+                    IMAGE(ELEMENTS,data.AppIcon||WHITEFOLDERICON,'50%','70%','',()=>{
+    
+                    });
+    
+                    CENTERTEXT(ELEMENTS,'',data.AppName,'','18px','',()=>{
+    
+                    });
+        
+                });
+
+            });
+    
+        });
+
+    },()=>{
+
+        CLEAR(ELEMENT);
+
+        GETINDEXEDDATA ('AppManager', 'AppManager', (data)=>{
+
+            CHECKER( data.Owner === Name && !data.AppDeleted ,()=>{
+
+                VIEW(ELEMENT,' ','46%','35%','',(ELEMENTS)=>{
+    
+                    STYLED(ELEMENTS,'display','inline-table');
+                    STYLED(ELEMENTS,'margin','1.5%');
+                    STYLED(ELEMENTS,'border','1px solid forestgreen');
+        
+                    ICON(ELEMENTS,WHITEPENCILICON,'25px','25px','',(ELEMENTSE)=>{
+        
+                        STYLED(ELEMENTSE,'position','absolute');
+                        STYLED(ELEMENTSE,'top','5%');
+                        STYLED(ELEMENTSE,'right','5%');
+                        STYLED(ELEMENTSE,'background','forestgreen');
+                        STYLED(ELEMENTSE,'padding','2%');
+                        STYLED(ELEMENTSE,'border-radius','5px');
+        
+                        CLICK(ELEMENTSE,()=>{
+
+                            JSONIFICATION(data,(MyData)=>{
+
+                                STOREDATA('','MyProject',MyData);
+
+                                ROUTE(' ',UPDATEPROJECTPAGE,'HOMEPAGE');
+
+                            });
+
+                        });
+                
+                    });
+        
+                    ICON(ELEMENTS,WHITEDELETEICON,'25px','25px','',(ELEMENTSE)=>{
+        
+                        STYLED(ELEMENTSE,'position','absolute');
+                        STYLED(ELEMENTSE,'top','5%');
+                        STYLED(ELEMENTSE,'left','5%');
+                        STYLED(ELEMENTSE,'background','transparent');
+                        STYLED(ELEMENTSE,'padding','2%');
+                        STYLED(ELEMENTSE,'border-radius','5px');
+        
+                        CLICK(ELEMENTSE,()=>{
+
+                            CONDITION(navigator.onLine,()=>{
+
+                                MESSAGEDISPLAY('',`${data.AppName} is Being Deleted`,'');
+        
+                                const INFO=[data.AppName,data.AppDescription,data.AppColors,data.AppConfiguration,data.AppCreatedOn,data.AppVersion,'Deleted',data.AppKeyWord,data.AppPackageName,data.AppCompany,data.AndroidDesign,data.AndroidFunctions,data.DesktopDesign,data.DesktopFunctions,data.WebDesign,data.WebFunctions,data.SharedDesign,data.SharedFunctions,data.AppLogic,data.AppRegion,data.AppState,data.AppCatergory,data.AppIcon,data.UpdatedOn,data.Owner];
+        
+                                UPDATEDATA(API,'APPMANAGER',data.ID,INFO,()=>{
+        
+                                    APPDOWNLOAD(()=>{
+        
+                                        ROUTE('',HOMEPAGE,'HOMEPAGE');
+        
+                                    });
+        
+                                },()=>{
+        
+                                        MESSAGEDISPLAY('','Failed to Delete App','');
+        
+                                    }
+        
+                                );
+
+                            },()=>{
+
+                                NOINTERNETTEMPLATE();
+
+                            });
+        
+                        });
+                
+                    });
+            
+                    IMAGE(ELEMENTS,data.AppIcon||WHITEFOLDERICON,'50%','70%','',()=>{
+        
+                    });
+        
+                    CENTERTEXT(ELEMENTS,'',data.AppName,'','18px','',(ELEMENTSE)=>{
+
+                        CLICK(ELEMENTSE,()=>{
+
+                            JSONIFICATION(data,(MyData)=>{
+
+                                STOREDATA('','MyProject',MyData);
+    
+                                ROUTE(' ',MYPROJECTDETAILSPAGE,'HOMEPAGE');
+    
+                            });
+
+                        });
+
+                    });
+        
+                });
+
+            });
+
+        });
+
+    });
+ 
 };
