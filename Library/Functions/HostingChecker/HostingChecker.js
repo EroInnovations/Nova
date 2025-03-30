bc9d2a3b12f43ee52aae7bacb33623a963e2e9b0
EroInnovations <eroinnovations9@gmail.com>
2025-03-30 11:01:49 +0300

    Hosting Updates

--- Library/Functions/HostingChecker/HostingChecker.js
+++ Library/Functions/HostingChecker/HostingChecker.js
@@ -6,29 +6,37 @@ const HOSTINGCHECKER=(callback)=>{
         
     } else {
 
-        DISPLAY('',`
+        if (localStorage.getItem('Environment') ==='Development' ) {
 
-            <h1 style="color: red;">Alert</h1>
-
-            <h2>Service Hosting </h2>
-
-            <p>The Hosting Service to This Product has Expired 
-
-            <br><br>
+            callback();
             
-            If your the Owner Please Pay To Keep It Online</p>
-            
-            <button style="width: 50%;height: 50px;background: blue;outline: none;border: none;color: #cdcdcd;margin: 2% auto;border-radius: 50px;">Pay Now</button>
+        }else{
 
-            <button style="width: 50%;height: 50px;background: forestgreen;outline: none;border: none;color: #cdcdcd;margin: 2% auto;border-radius: 50px;">Contact Us</button>
+            DISPLAY('',`
 
-            <p>Powered by</p>
+                <h1 style="color: red;">Alert</h1>
+    
+                <h2>Service Hosting </h2>
+    
+                <p>The Hosting Service to This Product has Expired 
+    
+                <br><br>
+                
+                If your the Owner Please Pay To Keep It Online</p>
+                
+                <button style="width: 50%;height: 50px;background: blue;outline: none;border: none;color: #cdcdcd;margin: 2% auto;border-radius: 50px;">Pay Now</button>
+    
+                <button style="width: 50%;height: 50px;background: forestgreen;outline: none;border: none;color: #cdcdcd;margin: 2% auto;border-radius: 50px;">Contact Us</button>
+    
+                <p>Powered by</p>
+    
+                <h3>Elite Robust Ontology</h2>
+    
+            `);
 
-            <h3>Elite Robust Ontology</h2>
-
-        `);
+        };
         
-    }
+    };
 
 };
 
