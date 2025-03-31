e94b449f63f8d5de42ea66f5157b21481a386def
EroInnovations <eroinnovations9@gmail.com>
2025-03-31 23:42:06 +0300

    Update Start.js
    New Message Of Expired Domain

--- Model/Start.js
+++ Model/Start.js
@@ -85,24 +85,40 @@ const NOVASTARTER=()=>{
                     body.innerHTML=`
         
                         <h1 style="color: red;">Alert</h1>
-
+            
                         <h2>Service Hosting </h2>
-
+            
                         <p>The Hosting Service to This Product has Expired 
-
+            
                         <br><br>
                         
                         If your the Owner Please Pay To Keep It Online</p>
                         
                         <button style="width: 50%;height: 50px;background: blue;outline: none;border: none;color: #cdcdcd;margin: 2% auto;border-radius: 50px;">Pay Now</button>
+            
+                        <button style="width: 50%;height: 50px;background: forestgreen;outline: none;border: none;color: #cdcdcd;margin: 2% auto;border-radius: 50px;">Contact Us</button>
+            
+                        <p>If You Think There Is a Mistake</p>
 
-                        <button style="width: 50%;height: 50px;background: orange;outline: none;border: none;color: #cdcdcd;margin: 2% auto;border-radius: 50px;">Contact Us</button>
-
+                        <button style="width: 50%;height: 50px;background: teal;outline: none;border: none;color: #cdcdcd;margin: 2% auto;border-radius: 50px;" class='DeleteAllData' >Update Site</button>
+            
                         <p>Powered by</p>
-
+            
                         <h3>Elite Robust Ontology</h2>
                     
                     `;
+
+                    const DeleteAllData=document.querySelector('.DeleteAllData');
+
+                    DeleteAllData.addEventListener('click',()=>{
+        
+                        localStorage.clear();
+        
+                        sessionStorage.clear();
+        
+                        Reload();
+        
+                    });
 
                     return
                     
