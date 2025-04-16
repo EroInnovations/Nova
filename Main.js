ddb4c79f8e9c1a03f43de3eb33638e8a7753d920
EroInnovations <eroinnovations9@gmail.com>
2025-04-16 08:33:40 +0300

    New First Loading Pages Introduced
    Ready to Continue Developing The Platform

---
+++ Main.js
@@ -0,0 +1,21 @@
+import { CENTERTEXT } from "./Library/Components/CenterText/CenterText.js";
+import { APPMODE } from "./Library/Functions/AppMode/AppMode.js";
+import { BODY } from "./Library/Functions/Body/Body.js";
+import { CLEAR } from "./Library/Functions/Clear/Clear.js";
+import { OPERATINGSYSTEM } from "./Library/Functions/OperatingSystem/OperatingSystem.js";
+
+OPERATINGSYSTEM();
+BODY();
+APPMODE('#000');
+
+const NOVASTART=()=>{
+
+    CLEAR('');
+
+    CENTERTEXT('','','ELite Robust Ontology','red','30px','10% auto',()=>{
+
+    });
+
+};
+
+NOVASTART();
