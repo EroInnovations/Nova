1873461eba2fc6a4a295f72c928372cce7a77caf
Elite Robust Ontology <eroinnovations9@gmail.com>
2025-04-19 13:09:29 +0300

    Update Android.js
    We have Launched The Home Page and Ready To Make it Launch

--- Site/Android.js
+++ Site/Android.js
@@ -1,12 +1,15 @@
 import { LEFTTEXT } from "../Library/Components/LeftText/LeftText.js"
+import { LEFTVIEW } from "../Library/Components/LeftView/LeftView.js";
 import { RIGHTTEXT } from "../Library/Components/RightText/RightText.js";
+import { TEXT } from "../Library/Components/Text/Text.js";
+import { VIEW } from "../Library/Components/View/Views.js";
 import { HOMEHEADERTEMPLATE } from "../Library/Templates/Components/HomeHeaderTemplate/HomeHeaderTemplate.js"
 
 const ANDROIDSITE=()=>{
 
     HOMEHEADERTEMPLATE('',' ',' ',(ELEMENT)=>{
 
-        LEFTTEXT(ELEMENT,'h3','Elite Robust Ontology','','18px','',()=>{
+        LEFTTEXT(ELEMENT,'h3','Elite Robust Ontology','','18px','1%','',()=>{
 
         });
 
@@ -15,6 +18,20 @@ const ANDROIDSITE=()=>{
         });
 
     },(ELEMENTS)=>{
+
+        LEFTTEXT(ELEMENTS,'h1','Ventures','','20px','10%','5% auto',()=>{
+
+        });
+
+        LEFTVIEW(ELEMENTS,' ','50%','50%','1% 2%',(COMMENT)=>{
+
+            const Messag=`Located At the Heart of Mbale City ,Elite Robust Ontology is a Software Company that has specialized in the Following Sectors in Information and Technology.`;
+
+            LEFTTEXT(COMMENT,'',Messag,'','18px','2%','5% auto',()=>{
+
+            });
+
+        })
 
     });
     
