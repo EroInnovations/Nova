e4096b9fde3618c0407b039e7d8f0deaf45fbb55
Elite Robust Ontology <eroinnovations9@gmail.com>
2025-04-24 19:54:51 +0300

    New Features
    We have New Test

--- Library/Functions/AppMode/AppMode.js
+++ Library/Functions/AppMode/AppMode.js
@@ -1,13 +1,10 @@
 const APPMODE = (COLOR) => {
     const body = document.querySelector("body");
 
-    // Fallback color
     body.style.background = COLOR || '#cdcdcd';
 
-    // If in production and AndroidColors interface is available
-    if (localStorage.getItem('Enviroment') === 'Production') {
-        changeWebViewBackground(COLOR||'#000000')
-    }
+    changeWebViewBackground(COLOR);
+    
 };
 
 export { APPMODE };
