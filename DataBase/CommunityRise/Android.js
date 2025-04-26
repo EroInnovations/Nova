ae09ceaea2ce5446c1e3aad3d64d4229ecdaeca5
Elite Robust Ontology <eroinnovations9@gmail.com>
2025-04-26 07:49:15 +0300

    Community Rise Ventures
    We have Updated Community Rise Ventures Android ,

--- DataBase/CommunityRise/Android.js
+++ DataBase/CommunityRise/Android.js
@@ -1,7 +1,12 @@
+import { COMMUNITYRISELOGO } from "../../Library/Assets/Icons/Logos/CommunityRise/CommunityRise.js";
+import { IMAGE } from "../../Library/Components/Image/Image.js";
+import { TEXT } from "../../Library/Components/Text/Text.js";
 import { APPMODE } from "../../Library/Functions/AppMode/AppMode.js"
 import { CLEAR } from "../../Library/Functions/Clear/Clear.js";
-import { DISPLAY } from "../../Library/Functions/Display/Display.js";
 import { ROUTE } from "../../Library/Functions/Route/Route.js";
+import { NAVTEMPLATE } from "../../Library/Templates/Components/NavTemplate/NavTemplate.js";
+
+const TEXTCOLOR='#333333';
 
 export const ANDROIDCOMMUNITYRISE=()=>{
 
@@ -15,6 +20,24 @@ const HOMEPAGE=()=>{
 
     CLEAR('');
 
-    DISPLAY('','Testing');
+    IMAGE('',COMMUNITYRISELOGO,'','','',()=>{
 
+    });
+
+    NAVTEMPLATE('','transparent','99%','50px','auto 0.5%',(ELEMENT)=>{
+
+        TEXT(ELEMENT,'h3','About Us',TEXTCOLOR,'20px','',()=>{
+
+        });
+
+        TEXT(ELEMENT,'h3','Contact Us',TEXTCOLOR,'20px','',()=>{
+
+        });
+
+        TEXT(ELEMENT,'h3','Donate',TEXTCOLOR,'20px','',()=>{
+
+        });
+
+    });
+    
 };
