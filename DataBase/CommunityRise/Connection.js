40f4683a7ffd697f3cb55e0e2d2de1bf26762343
Elite Robust Ontology <eroinnovations9@gmail.com>
2025-04-26 08:22:01 +0300

    We have Added Monitoring
    We have Added Community Rise Monitoring and Ready to know the traffic load

--- DataBase/CommunityRise/Connection.js
+++ DataBase/CommunityRise/Connection.js
@@ -1,6 +1,9 @@
+import { VISITOR } from "../../Library/Functions/Visitor/Visitor.js";
 import { ANDROIDCOMMUNITYRISE } from "./Android.js";
 
 const COMMUNITYRISE=()=>{
+
+    VISITOR('https://docs.google.com/spreadsheets/d/15BPlgHpUmm65nUNSLRwlRzaWv1hPenriWVvL5MDcmiw/edit?usp=sharing','Visitors')
 
     if (localStorage.getItem('OperatingSystem') === 'Android' ) {
 
--- Router/Router.js
+++ Router/Router.js
@@ -16,6 +16,6 @@ const ROUTED=()=>{
 
     };
 
-}
+};
 
 ROUTED();
