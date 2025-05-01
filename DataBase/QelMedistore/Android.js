4fad59d578bf663d03a1c6446d3745c6c4632c5f
Elite Robust Ontology <eroinnovations9@gmail.com>
2025-05-01 17:46:17 +0300

    Qel Launch
    We have Offically Launched The Qel App on The First

--- DataBase/QelMedistore/Android.js
+++ DataBase/QelMedistore/Android.js
@@ -1,49 +1,79 @@
+import { DOWNLOADICON } from "../../Library/Assets/Icons/WhiteIcons/DownloadIcon/DownloadIcon.js";
+import { BUTTONIMAGE } from "../../Library/Components/ButtonImage/ButtonImage.js";
 import { CENTERTEXT } from "../../Library/Components/CenterText/CenterText.js";
 import { IMAGE } from "../../Library/Components/Image/Image.js";
+import { LEFTTEXT } from "../../Library/Components/LeftText/LeftText.js";
 import { LEFTVIEW } from "../../Library/Components/LeftView/LeftView.js";
 import { RIGHTVIEW } from "../../Library/Components/RightView/RightView.js";
 import { VIEW } from "../../Library/Components/View/Views.js";
 import { CLEAR } from "../../Library/Functions/Clear/Clear.js";
+import { DOWNLOADIMAGE } from "../../Library/Functions/DownloadFile/DownloadFile.js";
+import { STYLED } from "../../Library/Functions/Style/Style.js";
 
 export const QELANDROIDSEITE=()=>{
 
     CLEAR();
 
-    IMAGE('',QELMODENORMAL,'100%','300px','',()=>{
+    CENTERTEXT('','h3','Qel Medistore','','30px','10% auto auto',()=>{
+        
+    })
 
+    CENTERTEXT('','h3','Enhancing Logistics','','18px','1% auto',()=>{
+        
+    })
+
+    CENTERTEXT('','h3','For Your Daily Medical and Laboratory Equipments','','15px','1% auto',()=>{
+        
     });
 
-    CENTERTEXT('','h1','Qel Medistore','','30px','10% auto auto',()=>{
+    VIEW('','transparent','90%','340px','3%',(ELEMENT)=>{
 
-    });
+        STYLED(ELEMENT,'border-radius','20px');
+        STYLED(ELEMENT,'border','1px solid #000000');
+        STYLED(ELEMENT,'overflow','hidden');
 
-    CENTERTEXT('','h1','Enhancing Logistics','','20px','1% auto',()=>{
+        const TEXTED='Qel Medsitaore is a Fully Registered and Approved Company In Uganda that Ventures in Selling of Medical and Laboratory Equipments ,Located In Kampala Uganda and has Branchies Around Uganda and Has Mobile App Where You Get Medical Supplies Today,<br><br> Qel Medistore Sells in Bulk For Large Business Owners and Also Those In Retail Not Forgetting Those Customers For are Purchasing For Personal Use,We got Your Covered.';
 
-    });
-
-
-    VIEW('','red','95%','250px','3%',(ELEMENT)=>{
-
-        LEFTVIEW(ELEMENT,'orange','60%','100%','',()=>{
-
-        });
-
-        RIGHTVIEW(ELEMENT,'black','40%','100%','',()=>{
-
-        });
-
-    });
-
-    VIEW('','red','95%','250px','3%',(ELEMENT)=>{
-
-        LEFTVIEW(ELEMENT,'orange','40%','100%','',()=>{
-
-        });
-
-        RIGHTVIEW(ELEMENT,'black','60%','100%','',()=>{
+        LEFTTEXT(ELEMENT,'',TEXTED,'','','2%','5%',()=>{
 
         });
 
     });
+
+    VIEW('','transparent','50%','500px','10%',(ELEMENT)=>{
+
+        STYLED(ELEMENT,'border-radius','20px');
+        STYLED(ELEMENT,'border','10px solid #000000');
+        STYLED(ELEMENT,'overflow','hidden');
+
+        IMAGE(ELEMENT,'https://eroinnovations.site/Assets/Images/QelMedistore/Qel App.png','100%','100%','','',()=>{
+
+        });
+
+    });
+
+    CENTERTEXT('','h1','Qel App','','25px','',()=>{
+
+    });
+
+    VIEW('','transparent','90%','100px','3%',(ELEMENT)=>{
+
+        STYLED(ELEMENT,'border-radius','20px');
+        STYLED(ELEMENT,'border','5px solid #000000');
+        STYLED(ELEMENT,'overflow','hidden');
+
+        const TEXTED='Download the Qel App and Start Showing Today For Your Business or Personal Usage Products';
+
+        LEFTTEXT(ELEMENT,'',TEXTED,'','','2%','5%',()=>{
+
+        });
+
+    });
+
+    BUTTONIMAGE('','forestgreen','Download App','',DOWNLOADICON,'50px','',()=>{
+        
+    });
+
     
+ 
 };        
