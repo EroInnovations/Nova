2c8025253a8355a873dfd7873deda82be88f101a
Elite Robust Ontology <eroinnovations9@gmail.com>
2025-05-01 18:48:40 +0300

    Update Connection.js
    Api added

--- DataBase/QelMedistore/Connection.js
+++ DataBase/QelMedistore/Connection.js
@@ -4,7 +4,6 @@ import { QELANDROIDSEITE } from "./Android.js";
 
 const API='https://docs.google.com/spreadsheets/d/18BUoCg4yVUrxWv8RG885ZIn2fjkURGgjIrCi6otCxFk/edit?usp=sharing';
 
-
 const QELCONNECTION=()=>{
 
     VISITOR(API,'Visitors');
