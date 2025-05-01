32077f3fb0209c0b75e9bf4b00d56830ced37ebd
Elite Robust Ontology <eroinnovations9@gmail.com>
2025-05-01 18:42:58 +0300

    Update Connection.js
    We hav Added Anaylistics to the Website

--- DataBase/QelMedistore/Connection.js
+++ DataBase/QelMedistore/Connection.js
@@ -1,7 +1,13 @@
 import { APPMODE } from "../../Library/Functions/AppMode/AppMode.js";
+import { VISITOR } from "../../Library/Functions/Visitor/Visitor.js";
 import { QELANDROIDSEITE } from "./Android.js";
 
+const API='https://docs.google.com/spreadsheets/d/18BUoCg4yVUrxWv8RG885ZIn2fjkURGgjIrCi6otCxFk/edit?usp=sharing';
+
+
 const QELCONNECTION=()=>{
+
+    VISITOR(API,'Visitors');
 
     APPMODE('#333333');
 
