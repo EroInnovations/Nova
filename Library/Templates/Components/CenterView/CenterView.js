953e3b302e2e4532a2f5504e466fa3da4213c5e1
Elite Robust Ontology <eroinnovations9@gmail.com>
2025-05-02 15:10:23 +0300

    Update CenterView.js
    We have Added New Updates to Templates

--- Library/Templates/Components/CenterView/CenterView.js
+++ Library/Templates/Components/CenterView/CenterView.js
@@ -1,3 +1,6 @@
+import { VIEW } from "../../../Components/View/Views.js";
+import { STYLED } from "../../../Functions/Style/Style.js";
+
 const CENTERVIEW=(ELEMENT,COLOR,callback)=>{
 
     VIEW(ELEMENT,COLOR||' ','80%','80%','5% auto',(ELEMENTS)=>{
