3f01bdc28b57792837a608fa4ebef835495a807a
eroinnovations <eroinnovations9@gmail.com>
2025-05-08 21:09:47 +0300

    Visitor We have Updated the Site

--- Library/Functions/Visitor/Visitor.js
+++ Library/Functions/Visitor/Visitor.js
@@ -16,56 +16,70 @@ const VISITOR=(API,NAME)=>{
 
     },()=>{
 
-        DEVICE((data)=>{
+        if (sessionStorage.getItem('Visited')) {
 
-            CHECKER(navigator.onLine,()=>{
-    
-                GETDATA(API,NAME,(MyData)=>{
-    
-                    FINDER(MyData,'ID',localStorage.getItem('ID'),(Users)=>{
-    
-                        CONDITION(Users.ID === localStorage.getItem('ID'),()=>{
-    
-                            JSONADDER ( Users.RevistDate,[new Date()],(dataDat)=>{
-    
-                                const INFO=[data,Users.Date,Users.Language,Users.DeviceScreen,dataDat,Users.VistedTimes+1,localStorage.getItem('OperatingSystem')];
-                            
-                                UPDATEDATA(API,NAME,Users.ID,INFO,(datata)=>{
-                
-                                },()=>{
-                
-                                });
-    
-                            });
-    
-                        },()=>{
-                            
-                            JSONADDER ( new Date(),[new Date()],(dataDat)=>{
-    
-                                const HEADERS=['Users','Date','Language','DeviceScreen','RevistDate','VistedTimes','OperatingSystem'];
-    
-                                const INFO=[data,new Date(),data.language,data.screen,dataDat,1,localStorage.getItem('OperatingSystem')];
-                                
-                                INSERTDATA(API,NAME,HEADERS,INFO,(datata)=>{
+            console.log('Already Visited');
+            
+        }else{
+
+            DEVICE((data)=>{
+
+                CHECKER(navigator.onLine,()=>{
         
-                                    STOREDATA(' ','ID',datata.uniqueId);
-                
-                                },()=>{
-                
+                    GETDATA(API,NAME,(MyData)=>{
+        
+                        FINDER(MyData,'ID',localStorage.getItem('ID'),(Users)=>{
+        
+                            CONDITION(Users.ID === localStorage.getItem('ID'),()=>{
+        
+                                JSONADDER ( Users.RevistDate,[new Date()],(dataDat)=>{
+        
+                                    const INFO=[data,Users.Date,Users.Language,Users.DeviceScreen,dataDat,Users.VistedTimes+1,localStorage.getItem('OperatingSystem')];
+                                
+                                    UPDATEDATA(API,NAME,Users.ID,INFO,(datata)=>{
+
+                                        STOREDATA('','Visited','true');
+                    
+                                    },()=>{
+                    
+                                    });
+        
                                 });
-    
-                            });
-    
-                        } );                  
-                    });
-    
-                },()=>{
-    
-                })
-    
+        
+                            },()=>{
+                                
+                                JSONADDER ( new Date(),[new Date()],(dataDat)=>{
+        
+                                    const HEADERS=['Users','Date','Language','DeviceScreen','RevistDate','VistedTimes','OperatingSystem'];
+        
+                                    const INFO=[data,new Date(),data.language,data.screen,dataDat,1,localStorage.getItem('OperatingSystem')];
+                                    
+                                    INSERTDATA(API,NAME,HEADERS,INFO,(datata)=>{
+            
+                                        STOREDATA(' ','ID',datata.uniqueId);
+
+                                        STOREDATA('','Visited','true');
+                    
+                                    },()=>{
+                    
+                                    });
+        
+                                });
+        
+                            } );                  
+                        });
+        
+                    },()=>{
+        
+                    })
+        
+                });
+        
             });
-    
-        });
+
+        }
+
+
 
     });
 
