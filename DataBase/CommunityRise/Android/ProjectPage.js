5ea0621cacbbe0957ea5a59f55cf066cca75a8b6
eroinnovations <eroinnovations9@gmail.com>
2025-05-09 06:23:07 +0300

    Projects Added Soon News

--- DataBase/CommunityRise/Android/ProjectPage.js
+++ DataBase/CommunityRise/Android/ProjectPage.js
@@ -1,6 +1,10 @@
 import { LEFTTEXTBACKHEADERBODY } from "../../../Library/Templates/Components/LeftTextBackHeaderBody/LeftTextBackHeaderBody.js";
 import { ROUTE } from "../../../Library/Functions/Route/Route.js";
 import { GETINDEXEDDATA } from "../../../Library/Functions/GetIndexedData/GetIndexedData.js";
+import { TABLEVIEW } from "../../../Library/Components/TableView/TabeView.js";
+import { IMAGE } from "../../../Library/Components/Image/Image.js";
+import { FOOTER } from "../../../Library/Components/Footer/Footer.js";
+import { TEXT } from "../../../Library/Components/Text/Text.js";
 const TEXTCOLOR='#ffffff';
 export const PROJECTPAGE=()=>{
 
@@ -15,6 +19,22 @@ export const PROJECTPAGE=()=>{
         GETINDEXEDDATA('News','News',(data)=>{
 
             console.log(data);
+
+           TABLEVIEW(ELEMENT,'transparent','190px','285px','','auto 1%',(ELEMENTS)=>{
+        
+            IMAGE(ELEMENTS,data.ImageOne||COMMUNITYRISELOGO,'100%','100%','',()=>{
+        
+            });
+
+            FOOTER(ELEMENTS,'#000000','','50px',(ELEMENTES)=>{
+        
+                TEXT(ELEMENTES,'',data.Name||'Community Rise Ventures','white','20px','',()=>{
+        
+                });
+        
+            });
+        
+        });
 
         });
 
